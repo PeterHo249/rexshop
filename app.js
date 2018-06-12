b91284a3e3c66f2a64738e3761aa647467fd20fd
denguyen5697 <ntdvt197@gmail.com>
2018-06-13 04:04:59 +0700

    add 2 hbs file

--- app.js
+++ app.js
@@ -21,20 +21,20 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator({
-  customValidators: {
-    isEqual: (value1, value2) => {
-      return value1 === value2;
+    customValidators: {
+        isEqual: (value1, value2) => {
+            return value1 === value2;
+        }
     }
-  }
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -43,10 +43,10 @@ var passport = require('passport');
 require('./config/passport')(passport);
 var expressSession = require('express-session');
 app.use(expressSession({
-  secret: 'mysecretkey',
-  resave: false,
-  saveUninitialized: true
-  }));
+    secret: 'mysecretkey',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -56,35 +56,34 @@ app.use(flash());
 
 // Config mailer
 mailer.extend(app, {
-  from: 'no-reply@rexshop.com',
-  host: 'smtp.gmail.com',
-  secureConnection: true,
-  port: 465,
-  transportMethod: 'SMTP',
-  auth: {
-    user: 'peterho951357@gmail.com',
-    pass: 'thanchetpro123'
-  }
+    from: 'no-reply@rexshop.com',
+    host: 'smtp.gmail.com',
+    secureConnection: true,
+    port: 465,
+    transportMethod: 'SMTP',
+    auth: {
+        user: 'peterho951357@gmail.com',
+        pass: 'thanchetpro123'
+    }
 });
 
 require('./routes/index')(app, passport);
 require('./routes/product')(app, passport);
 require('./routes/user')(app, passport);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,2 +1,160 @@
 /* jshint esversion: 6 */
 let Order = require('../models/order');
+
+let async = require('async');
+
+exports.list_order = function(req, res) {
+    async.parallel({
+        orders: function(callback) {
+            Order.find({}).exec(callback);
+        }
+    }, function(err, results) {
+        if (err) {
+            return next(err);
+        }
+        if (results.orders == null) {
+            let err = new Error('Order not found');
+            err.status = 404;
+            return next(err);
+        }
+        if (req.user) {
+            res.render('order', {
+                title: 'RexShop',
+                user: req.user,
+                order_items: results.orders,
+                salesman: true
+            });
+        } else {
+            res.render('order', {
+                title: 'RexShop',
+                order_items: results.orders,
+                salesman: true
+            });
+        }
+
+    });
+};
+
+exports.show_order_detail = function(req, res) {
+    async.parallel({
+        order: function(callback) {
+            Order.findById(req.params.id).exec(callback);
+        }
+    }, function(err, results) {
+        if (err) {
+            return next(err);
+        }
+        if (results.product == null) {
+            let err = new Error('Order not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        if (req.user) {
+            res.render('order_detail', {
+                title: 'RexShop',
+                user: req.user,
+                order_items: results.order,
+                salesman: true
+            });
+        } else {
+            res.render('order_detail', {
+                title: 'RexShop',
+                order_item: results.order,
+                salesman: true
+            });
+        }
+
+    });
+};
+
+exports.edit_order = function(req, res) {
+    async.parallel({
+        order: function(callback) {
+            Order.findOne({ _id: req.params.id }).exec(callback);
+        }
+    }, function(err, results) {
+        if (err) {
+            return next(err);
+        }
+        if (results.product == null) {
+            let err = new Error('Order not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        //Render
+        if (req.user) {
+            res.render('order_edit', {
+                title: 'RexShop',
+                user: req.user,
+                order_item: results.order,
+                salesman: true
+            });
+        } else {
+            res.render('order_edit', {
+                title: 'RexShop',
+                order_item: results.order,
+                salesman: true
+            });
+        }
+    });
+};
+
+exports.update_order = function(req, res) {
+    async.parallel({
+        order: function(callback) {
+            Order.findByIdAndUpdate(req.params.id, {
+                $set: {
+                    staff_id: req.body.staff_id,
+                    customer_id: req.body.customer_id,
+                    cost: req.body.cost,
+                    date: req.body.date,
+                    status: req.body.status,
+                    shopping: req.body.shopping,
+                    count: req.body.count
+                }
+            }, { new: false }, function(err, result) {
+                if (err) {
+                    console.log(err);
+                }
+
+                res.redirect("/salesman/order/" + order._id);
+            });
+        }
+    });
+};
+
+exports.delete_order = function(req, res) {
+    async.parallel({
+        function(callback) {
+            Order.findByIdAndRemove({ _id: req.params.id }, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Deleted");
+                    res.redirect("/salesman/order");
+                }
+            });
+        }
+    });
+};
+
+
+exports.create_order = function(req, res) {
+    res.render('order_add', {
+        salesman: true
+    })
+};
+
+exports.order_save = function(req, res) {
+    var order = new Order(req.body);
+    order.save(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Created");
+            res.redirect("/salesman/order/" + order._id);
+        }
+    });
+};
