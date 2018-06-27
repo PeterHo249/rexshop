7045160130338602f82c25102142aef7d780979f
PETERHO\PeterHo <peter_ho_249@outlook.com>
2018-06-27 21:29:56 +0700

    add checking login middleware

--- routes/admin.js
+++ routes/admin.js
@@ -9,34 +9,34 @@ module.exports = function(app, passport) {
     /// PRODUCT ROUTE ///
 
     // GET homepage
-    app.get('/admin', admin_controller.admin_index);
+    app.get('/admin', auth.is_logged_in('manager'), admin_controller.admin_index);
 
     // GET product catagories
-    app.get('/admin/manage_account', admin_controller.admin_manage_account);
+    app.get('/admin/manage_account', auth.is_logged_in('manager'), admin_controller.admin_manage_account);
 
     // GET product category and brand
-    app.get('/admin/manage_account/:id', admin_controller.admin_view_account);
+    app.get('/admin/manage_account/:id', auth.is_logged_in('manager'), admin_controller.admin_view_account);
 
     // GET product detail
-    app.get('/admin/manage_product/add', admin_controller.admin_add_new_product);
+    app.get('/admin/manage_product/add', auth.is_logged_in('manager'),admin_controller.admin_add_new_product);
 
-    app.get('/admin/manage_account/edit/:id', admin_controller.admin_edit_account);
+    app.get('/admin/manage_account/edit/:id', auth.is_logged_in('manager'), admin_controller.admin_edit_account);
 
-    app.post('/admin/manage_account/update/:id', admin_controller.admin_update_account);
+    app.post('/admin/manage_account/update/:id', auth.is_logged_in('manager'), admin_controller.admin_update_account);
 
-    app.post('/admin/manage_account/delete/:id', admin_controller.admin_detele_account);
+    app.post('/admin/manage_account/delete/:id', auth.is_logged_in('manager'), admin_controller.admin_detele_account);
 
-    app.get('/admin/manage_order', admin_controller.admin_manage_order);
+    app.get('/admin/manage_order', auth.is_logged_in('manager'), admin_controller.admin_manage_order);
 
-    app.get('/admin/manage_product', admin_controller.admin_manage_product);
+    app.get('/admin/manage_product', auth.is_logged_in('manager'), admin_controller.admin_manage_product);
 
-    app.get('/admin/manage_product/:id', admin_controller.admin_view_product);
+    app.get('/admin/manage_product/:id', auth.is_logged_in('manager'), admin_controller.admin_view_product);
 
-    app.get('/admin/manage_product/edit/:id', admin_controller.admin_edit_product);
+    app.get('/admin/manage_product/edit/:id', auth.is_logged_in('manager'), admin_controller.admin_edit_product);
 
-    app.post('/admin/manage_product/update/:id', admin_controller.admin_update_product);
+    app.post('/admin/manage_product/update/:id', auth.is_logged_in('manager'), admin_controller.admin_update_product);
 
-    app.post('/admin/manage_product/delete/:id', admin_controller.admin_delete_product);
+    app.post('/admin/manage_product/delete/:id', auth.is_logged_in('manager'), admin_controller.admin_delete_product);
 
-    app.post('/admin/manage_product/save', admin_controller.admin_save_product);
+    app.post('/admin/manage_product/save', auth.is_logged_in('manager'), admin_controller.admin_save_product);
 };
--- routes/order.js
+++ routes/order.js
@@ -1,5 +1,4 @@
-let express = require('express');
-let router = express.Router();
+
 let auth = require('../config/auth');
 
 // Require controller module
@@ -8,20 +7,20 @@ var order_controller = require('../controllers/orderController');
 module.exports = function(app, passport) {
     /// PRODUCT ROUTE ///
     // GET homepage
-    app.get('/salesman/order', order_controller.list_order);
+    app.get('/salesman/order', auth.is_logged_in('salesman'), order_controller.list_order);
 
     // GET product catagories
-    app.get('/salesman/order/add', order_controller.add_order);
+    app.get('/salesman/order/add', auth.is_logged_in('salesman'), order_controller.add_order);
 
     // GET product category and brand
-    app.get('/salesman/order/:id', order_controller.show_order);
+    app.get('/salesman/order/:id', auth.is_logged_in('salesman'), order_controller.show_order);
 
     // GET product detail
-    app.get('/salesman/order/edit/:id', order_controller.edit_order);
+    app.get('/salesman/order/edit/:id', auth.is_logged_in('salesman'), order_controller.edit_order);
 
-    app.post('/salesman/order/delete/:id', order_controller.delete_order);
+    app.post('/salesman/order/delete/:id', auth.is_logged_in('salesman'), order_controller.delete_order);
 
-    app.post('/salesman/order/update/:id', order_controller.update_order);
+    app.post('/salesman/order/update/:id', auth.is_logged_in('salesman'), order_controller.update_order);
 
-    app.post('/salesman/order/save', order_controller.save_order);
+    app.post('/salesman/order/save', auth.is_logged_in('salesman'), order_controller.save_order);
 };
